a8a6c5f352c5b4e6a0b0e0e523caa4ade212255f
ANTU SHIL <anturoy084@gmail.com>
2026-01-25 20:30:06 +0600

    added React router

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router';
+import MainLayout from '../layouts/MainLayout';
+import Homepage from '../pages/Homepage/Homepage';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: MainLayout,
+    children:[
+      {
+        index:true,
+        element: <Homepage></Homepage>
+      }
+    ]
   },
 ]);
 
