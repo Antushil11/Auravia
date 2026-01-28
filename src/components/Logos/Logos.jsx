5f2df46983597f3754f09b40e81355ad14b20bf0
ANTU SHIL <anturoy084@gmail.com>
2026-01-28 21:22:54 +0600

    added container navbar link coloor

--- src/components/Logos/Logos.jsx
+++ src/components/Logos/Logos.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Logos = () => {
     return (
-        <div>
+        <div className='text-4xl tinos-regular  text-secondary-content'>
+            Auravia
             
         </div>
     );
