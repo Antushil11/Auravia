3ef07ec2756c6f2f1843fd56dee752d3b1e102be
ANTU SHIL <anturoy084@gmail.com>
2026-01-31 19:43:48 +0600

    footer editing

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import Logos from "../Logos/Logos";
 const Footer = () => {
   return (
     <div>
-      <footer className="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
+      <footer className="footer sm:footer-horizontal bg-base-100 shadow-sm text-base-content p-10">
         <aside>
           <Logos></Logos>
           <p>
