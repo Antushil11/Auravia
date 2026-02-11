d1e27c7e2a26d209d68e1ad1734f744f1d073d61
ANTU SHIL <anturoy084@gmail.com>
2026-02-11 20:17:46 +0600

    added banner text added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,7 +6,13 @@ const Banner = () => {
        <Container>
          <div className=''>
            <img className='max-h-[600px] w-full' src="../../../public/image/luxurious skincare a.png" alt="" />
-         
+              
+              <div>
+               <h1>Auravia</h1>
+               <h4>Glow with Elegance</h4>
+               <p>Discover Luxurious Skincare & Beauty Products</p>
+            
+              </div>
         </div>
        </Container>
     );
