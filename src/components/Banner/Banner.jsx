61c3b28b9b491e481e064a5c13792a64b43b6d3d
ANTU SHIL <anturoy084@gmail.com>
2026-02-16 21:40:41 +0600

    added banner flex added text added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,34 +1,45 @@
-import React from 'react';
-import Container from '../../Container/Container';
+import React from "react";
+import Container from "../../Container/Container";
 
 const Banner = () => {
-    return (
-       <Container>
-         <div className=''>
-           
-              
-              <div>
-               <div className=''>
-
-                <img className='max-h-60 w-96' src="../../../public/image/flower.png" alt="" />
-               <div className='flex'>
-                 <img className='max-h-60 w-96' src="../../../public/image/product 1 .png" alt="" />
-                <img className='max-h-60 w-96' src="../../../public/image/product 2.png" alt="" />
-                <img className='max-h-60 w-96' src="../../../public/image/product 3.png" alt="" />
-               </div>
-                
-                
-               </div>
-               <div>
-                <h1>Auravia</h1>
-               <h4>Glow with Elegance</h4>
-               <p>Discover Luxurious Skincare & Beauty Products</p>
-               </div>
-            
-              </div>
+  return (
+    <Container>
+      <div className="flex items-center justify-center">
+        <div>
+          <h1>Auravia</h1>
+          <h4>Glow with Elegance</h4>
+          <p>Discover Luxurious Skincare & Beauty Products</p>
         </div>
-       </Container>
-    );
+
+        
+          <div className="">
+            <img
+              className="max-h-60 w-96"
+              src="../../../public/image/flower.png"
+              alt=""
+            />
+            <div className="flex">
+              <img
+                className="max-h-60 w-96"
+                src="../../../public/image/product 1 .png"
+                alt=""
+              />
+              <img
+                className="max-h-60 w-96"
+                src="../../../public/image/product 2.png"
+                alt=""
+              />
+              <img
+                className="max-h-60 w-96"
+                src="../../../public/image/product 3.png"
+                alt=""
+              />
+            </div>
+          </div>
+        
+      </div>
+    </Container>
+  );
 };
 
 export default Banner;
