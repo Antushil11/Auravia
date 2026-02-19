ed68afcdd483bf1a7d6f3b951dfe38dfa9f59346
ANTU SHIL <anturoy084@gmail.com>
2026-02-19 20:05:55 +0600

    added Banner text modify

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,8 +4,8 @@ import Container from "../../Container/Container";
 const Banner = () => {
   return (
     <Container>
-      <div className="flex items-center justify-center border-2 border-amber-800">
-        <div className="border-2 border-amber-800 h-96 flex flex-col items-center justify-center">
+      <div className="flex items-center justify-center gap-40 border-2 border-amber-800">
+        <div className="border-2 border-amber-800 w-[600px] h-96 flex flex-col items-center justify-center">
           <h1 className="text-4xl ">Auravia</h1>
           <h4>Glow with Elegance</h4>
           <p>Discover Luxurious Skincare & Beauty Products</p>
@@ -16,23 +16,23 @@ const Banner = () => {
         
           <div className="">
             <img 
-              className="max-h-60 w-96 ml-60 border-2 border-amber-800"
+              className="max-h-60 w-96 z-60 ml-96 border-2 border-amber-800"
               src="../../../public/image/flower.png"
               alt=""
             />
-            <div className="flex">
+            <div className="flex items-end">
               <img
-                className="max-h-60 w-96 border-2 border-amber-800"
+                className="max-h-960 w-96 border-2 border-amber-800"
                 src="../../../public/image/product 1 .png"
                 alt=""
               />
               <img
-                className="max-h-60 w-96 border-2 border-amber-800"
+                className="max-h-80 w-96 border-2 border-amber-800"
                 src="../../../public/image/product 2.png"
                 alt=""
               />
               <img
-                className="max-h-60 w-96 border-2 border-amber-800"
+                className="max-h-80 w-96 border-2 border-amber-800"
                 src="../../../public/image/product 3.png"
                 alt=""
               />
