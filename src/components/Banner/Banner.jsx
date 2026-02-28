6635b20f090b216f4dbd2a697a9d95ef7ea71526
ANTU SHIL <anturoy084@gmail.com>
2026-02-28 19:52:32 +0600

    added banner flex or element

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,8 +4,8 @@ import Container from "../../Container/Container";
 const Banner = () => {
   return (
     <Container>
-      <div className="flex items-center  justify-center gap-40 border-2 border-amber-800">
-        <div className="border-2 border-amber-800 w-full h-96 flex flex-col items-center justify-center">
+      <div className="md:flex items-center  justify-center gap-40 border-2 border-amber-800">
+        <div className="border-2 border-amber-800 text-center w-full h-96 flex flex-col items-center justify-center">
           <h1 className="text-4xl ">Auravia</h1>
           <h4>Glow with Elegance</h4>
           <p>Discover Luxurious Skincare & Beauty Products</p>
@@ -30,12 +30,12 @@ const Banner = () => {
               />
               
               <img
-                className="max-h-11/12 absolute ml-40 z-100 max-w-96  border-2 border-amber-800"
+                className="max-h-11/12 absolute  z-100 max-w-96  border-2 border-amber-800"
                 src="../../../public/image/product 2.png"
                 alt=""
               />
               <img
-                className="max-h-96 absolute   z-100 max-w-96 border-2 border-amber-800"
+                className="max-h-96 absolute md:ml-40  z-100 max-w-96 border-2 border-amber-800"
                 src="../../../public/image/product 3.png"
                 alt=""
               />
