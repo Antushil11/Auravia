d651078e36d044b63995332a24a8080e11c2739c
ANTU SHIL <anturoy084@gmail.com>
2026-03-21 21:44:29 +0600

    added about page

---
+++ src/components/About/About.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <div>
+        <h1>About Auravia</h1>
+        <h2>Luxury Skincare Crafted with Care</h2>
+        <p>
+          Autavia blends nature and science to create exceptional skincare
+          products that nourish and transform your skin. Our commitment to
+          quality, sustainability, and innovation drives us to deliver effective
+          solutions that enhance your natural beauty. Experience the difference
+          with Auravia, where every product is crafted with care to help you
+          achieve radiant, healthy skin.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Banner from '../../components/Banner/Banner';
+import About from '../../components/About/About';
+
 
 const Homepage = () => {
     return (
         <div> 
             <Banner></Banner>
+            <About></About>
+        
+            
             
         </div>
     );
