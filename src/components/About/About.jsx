94a667543810bbbecc1f05e2224cdffb1232c554
ANTU SHIL <anturoy084@gmail.com>
2026-04-04 21:21:32 +0600

    added about page button

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,17 +4,30 @@ import Container from "../../Container/Container";
 const About = () => {
   return (
     <Container>
-      <div className="max-w-[1500px] mx-auto">
-        <h1 className="text-6xl tinos-regular  text-neutral ">About Auravia</h1>
-        <h2>Luxury Skincare Crafted with Care</h2>
-        <p>
-          Autavia blends nature and science to create exceptional skincare
-          products that nourish and transform your skin. Our commitment to
-          quality, sustainability, and innovation drives us to deliver effective
-          solutions that enhance your natural beauty. Experience the difference
-          with Auravia, where every product is crafted with care to help you
-          achieve radiant, healthy skin.
-        </p>
+      <div className="grid grid-cols-2">
+        <div className="max-w-[1500px] mx-auto col-span-1 flex flex-col items-start gap-4">
+          <h1 className="text-6xl tinos-regular  text-neutral ">
+            About Auravia
+          </h1>
+          <h2 className="text-4xl tinos-regular text-neutral-content">
+            Luxury Skincare Crafted with Care
+          </h2>
+          <p className="text-accent-content tinos-regular text-xl ">
+            Autavia blends nature and science to create exceptional skincare
+            products that nourish and transform your skin. Our commitment to
+            quality, sustainability, and innovation drives us to deliver
+            effective solutions that enhance your natural beauty. Experience the
+            difference with Auravia, where every product is crafted with care to
+            help you achieve radiant, healthy skin.
+          </p>
+
+          <button className="btn text-white mr-2 md:mt-0 mt-4 p-6 tinos-regular text-xl px-10 bg-linear-to-r from-secondary-content to-accent-content rounded-xl">
+            Learn More
+          </button>
+        </div>
+        <div className="col-span-1">
+          
+        </div>
       </div>
     </Container>
   );
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -7,44 +7,48 @@ const Banner = () => {
       <div className="md:flex items-center bg-gradient-to-r from-[#f6eee7] via-[#efe1d4] to-[#e6d1bf] justify-center gap-40 border-2 border-amber-800">
         <div className="gap-4 w-full max-h-full flex flex-col items-center justify-center">
           <h1 className="text-6xl tinos-regular  text-neutral ">Auravia</h1>
-          <h4 className="text-4xl tinos-regular text-neutral-content">Glow with Elegance</h4>
-          <p className="text-accent-content tinos-regular text-xl text-center" >Discover Luxurious Skincare & Beauty Products</p>
+          <h4 className="text-4xl tinos-regular text-neutral-content">
+            Glow with Elegance
+          </h4>
+          <p className="text-accent-content tinos-regular text-xl text-center">
+            Discover Luxurious Skincare & Beauty Products
+          </p>
 
           <div className=" flex-col gap-4 text-center ">
-            <button className="btn text-white mr-2 md:mt-0 mt-4 p-6 tinos-regular text-xl px-10 bg-linear-to-r from-secondary-content to-accent-content rounded-xl">Shop Now</button>
-          <button className="btn text-accent-content md:mt-0 mt-4 p-6 tinos-regular text-xl px-10 border-2 border-accent-content rounded-xl">View Collection</button>
-        </div>
+            <button className="btn text-white mr-2 md:mt-0 mt-4 p-6 tinos-regular text-xl px-10 bg-linear-to-r from-secondary-content to-accent-content rounded-xl">
+              Shop Now
+            </button>
+            <button className="btn text-accent-content md:mt-0 mt-4 p-6 tinos-regular text-xl px-10 border-2 border-accent-content rounded-xl">
+              View Collection
+            </button>
           </div>
+        </div>
 
-        
-          <div className="relative w-full    ">
-            
-            <img 
-              className="h-full z-80 w-full absolute  "
-              src="../../../public/image/flower.png"
+        <div className="relative w-full    ">
+          <img
+            className="h-full z-80 w-full absolute  "
+            src="../../../public/image/flower.png"
+            alt=""
+          />
+          <div className="flex items-end relative">
+            <img
+              className="max-h-11/12 md:ml-14 z-100 md:max-w-96"
+              src="../../../public/image/product 1 .png"
               alt=""
             />
-            <div className="flex items-end relative">
-              
-              <img
-                className="max-h-11/12 md:ml-14 z-100 md:max-w-96"
-                src="../../../public/image/product 1 .png"
-                alt=""
-              />
-              
-              <img
-                className="max-h-11/12 absolute md:ml-40   z-100 md:max-w-96 "
-                src="../../../public/image/product 2.png"
-                alt=""
-              />
-              <img
-                className="max-h-96 absolute  z-100 md:max-w-96 "
-                src="../../../public/image/product 3.png"
-                alt=""
-              />
-            </div>
+
+            <img
+              className="max-h-11/12 absolute md:ml-40   z-100 md:max-w-96 "
+              src="../../../public/image/product 2.png"
+              alt=""
+            />
+            <img
+              className="max-h-96 absolute  z-100 md:max-w-96 "
+              src="../../../public/image/product 3.png"
+              alt=""
+            />
           </div>
-        
+        </div>
       </div>
     </Container>
   );
